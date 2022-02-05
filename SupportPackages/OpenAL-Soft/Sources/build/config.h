c9234f78d87c11468dfa5f65b5e0fa32522581c0
Hongtae Kim <tiff2766@gmail.com>
2022-02-05 22:49:49 +0900

    no message

---
+++ SupportPackages/OpenAL-Soft/Sources/build/config.h
@@ -0,0 +1,124 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define AL_ALEXT_PROTOTYPES
+
+#ifdef _WIN32
+#ifdef AL_LIBTYPE_STATIC
+#define AL_API
+#define ALC_API
+#else
+#define AL_API  __declspec(dllexport)
+#define ALC_API __declspec(dllexport)
+#endif
+
+#define ALIGN(x) __declspec(align(x))
+
+#define SIZEOF_LONG			4
+#define SIZEOF_LONG_LONG	8
+
+#define NOMINMAX
+#define RESTRICT __restrict
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define strcasecmp	_stricmp
+#define strncasecmp	_strnicmp
+
+
+#define HAVE__ALIGNED_MALLOC
+#ifdef __clang__
+#define HAVE_GCC_GET_CPUID
+#define HAVE_CPUID_H
+#endif
+#define HAVE_CPUID_INTRINSIC
+#define HAVE_SSE
+#define HAVE_SSE2
+#define HAVE_SSE3
+#define HAVE_SSE4_1
+#define HAVE_MMDEVAPI
+#define HAVE_DSOUND
+#define HAVE_WINMM
+#define HAVE_WAVE
+#define HAVE_WASAPI
+#define HAVE_STAT
+#define HAVE_STDINT_H
+#define HAVE_WINDOWS_H
+#define HAVE_XMMINTRIN_H
+#define HAVE_MALLOC_H
+#define HAVE_GUIDDEF_H
+#define HAVE_FLOAT_H
+#define HAVE__CONTROLFP
+#endif	/*fdef _WIN32*/
+
+#if defined(__APPLE__) && defined(__MACH__)
+// #include <TargetConditionals.h>
+// #if TARGET_OS_IPHONE		// iOS
+// #error OpenAL-soft not support iOS.
+// #endif
+
+#define AL_API  __attribute__((visibility("default")))
+#define ALC_API __attribute__((visibility("default")))
+
+/* Define any available alignment declaration */
+#define ALIGN(x) __attribute__((aligned(x)))
+
+#if __LP64__
+#define SIZEOF_LONG			8
+#define SIZEOF_LONG_LONG	8
+#else
+#define SIZEOF_LONG			4
+#define SIZEOF_LONG_LONG	8
+#endif
+
+#define restrict __restrict
+#define HAVE_POSIX_MEMALIGN
+#define HAVE_SSE
+#define HAVE_COREAUDIO
+#define HAVE_WAVE
+#define HAVE_STAT
+#define HAVE_LRINTF
+#define HAVE_STRTOF
+#define HAVE_GCC_DESTRUCTOR
+#define HAVE_GCC_FORMAT
+#define HAVE_STDINT_H
+#define HAVE_DLFCN_H
+#define HAVE_XMMINTRIN_H
+#define HAVE_CPUID_H
+#define HAVE_FLOAT_H
+#define HAVE_FENV_H
+#define HAVE_PTHREAD_SETSCHEDPARAM
+#endif	/*if defined(__APPLE__) && defined(__MACH__)*/
+
+#ifdef __ANDROID__
+#define AL_API  __attribute__((visibility("default")))
+#define ALC_API __attribute__((visibility("default")))
+
+/* Define any available alignment declaration */
+#define ALIGN(x) __attribute__((aligned(x)))
+
+#if __LP64__
+#define SIZEOF_LONG			8
+#define SIZEOF_LONG_LONG	8
+#else
+#define SIZEOF_LONG			4
+#define SIZEOF_LONG_LONG	8
+#endif
+
+#define restrict __restrict
+#define HAVE_ARM_NEON_H
+#define HAVE_OPENSL
+#define HAVE_WAVE
+#define HAVE_STAT
+#define HAVE_LRINTF
+#define HAVE_STRTOF
+#define HAVE_GCC_DESTRUCTOR
+#define HAVE_GCC_FORMAT
+#define HAVE_STDINT_H
+#define HAVE_DLFCN_H
+#define HAVE_XMMINTRIN_H
+#define HAVE_FLOAT_H
+#define HAVE_FENV_H
+#define HAVE_PTHREAD_SETSCHEDPARAM
+#endif /*ifdef __ANDROID__*/
+
+#endif /*ifndef CONFIG_H*/
