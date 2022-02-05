fe968241ca4809d852b562f0bbffb7f0b2659084
Hongtae Kim <tiff2766@gmail.com>
2022-02-05 22:10:19 +0900

    fixed OpenAL build errors

--- SupportPackages/OpenAL-Soft/Package.swift
+++ SupportPackages/OpenAL-Soft/Package.swift
@@ -15,14 +15,106 @@ let package = Package(
     targets: [
         .target(
             name: "OpenAL",
+            dependencies: [
+                .target(name: "backend"),
+                .target(name: "backend_windows", condition:.when(platforms: [.windows])),
+                .target(name: "mixer_sse", condition:.when(platforms: [.windows])),
+                // .target(name: "mixer_neon", condition:.when(platforms: [.iOS])),
+                ],
             path: "Sources",
+            exclude: [
+                "alc/backends",
+                "core/mixer/mixer_sse.cpp",
+                "core/mixer/mixer_sse2.cpp",
+                "core/mixer/mixer_sse3.cpp",
+                "core/mixer/mixer_sse41.cpp", 
+                "core/mixer/mixer_neon.cpp",               
+            ],
             sources: [
+                "common",
                 "al",
                 "alc",
-                "common",
                 "core",
             ],
-            publicHeadersPath: "include"),
+            publicHeadersPath: "include",
+            cSettings: [],
+            cxxSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("build"),
+                .headerSearchPath("common"),
+                .headerSearchPath("alc"),
+                //.define("AL_ALEXT_PROTOTYPES"),
+                //.unsafeFlags(["-fms-extensions"], .when(platforms: [.windows]))
+                .unsafeFlags(["-Wno-unused-value"])
+            ],
+            linkerSettings: [
+                .linkedLibrary("Shell32", .when(platforms: [.windows])),
+                .linkedLibrary("Ole32", .when(platforms: [.windows])),
+                .linkedLibrary("User32", .when(platforms: [.windows])),
+                .linkedLibrary("Winmm", .when(platforms: [.windows])),
+                .linkedLibrary("swiftCore"), // swift_addNewDSOImage
+            ]),
+        .target(
+            name: "mixer_sse",
+            path: "Sources",
+            sources: [
+                "core/mixer/mixer_sse.cpp",
+                "core/mixer/mixer_sse2.cpp",
+                "core/mixer/mixer_sse3.cpp",
+                "core/mixer/mixer_sse41.cpp",
+            ],
+            publicHeadersPath: "swift_umbrella/mixer_sse",
+            cxxSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("build"),
+                .headerSearchPath("common"),
+                ]),
+        .target(
+            name: "mixer_neon",
+            path: "Sources",
+            sources: [
+                "core/mixer/mixer_neon.cpp",
+            ],
+            publicHeadersPath: "swift_umbrella/mixer_neon",
+            cxxSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("build"),
+                .headerSearchPath("common"),
+                ]),
+        .target(
+            name: "backend",
+            path: "Sources",
+            sources: [
+                "alc/backends/loopback.cpp",
+                "alc/backends/base.cpp",
+                "alc/backends/null.cpp",
+                "alc/backends/wave.cpp",
+            ],
+            publicHeadersPath: "swift_umbrella/backend",
+            cxxSettings: [
+                .headerSearchPath("alc"),
+                .headerSearchPath("build"),
+                .headerSearchPath("common"),
+                .headerSearchPath("include"),
+                .headerSearchPath("."),
+                ]),
+        .target(
+            name: "backend_windows",
+            path: "Sources",
+            sources: [
+                "alc/backends/dsound.cpp",
+                "alc/backends/winmm.cpp",
+                "alc/backends/wasapi.cpp"
+            ],
+            publicHeadersPath: "swift_umbrella/backend_windows",
+            cxxSettings: [
+                .headerSearchPath("alc"),
+                .headerSearchPath("build"),
+                .headerSearchPath("common"),
+                .headerSearchPath("include"),
+                .headerSearchPath("."),
+                ])
     ],
-    cLanguageStandard: .c11
+    cLanguageStandard: .c11,
+    cxxLanguageStandard: .cxx20
 )
