7e42278b703ad2d4cc56fa06f8c7106d39b24457
Hongtae Kim <tiff2766@gmail.com>
2022-07-28 12:17:05 +0900

    aabb

--- Sources/DKGame/Collision/AABB.swift
+++ Sources/DKGame/Collision/AABB.swift
@@ -9,8 +9,16 @@ public struct AABB {
     public let min: Vector3
     public let max: Vector3
 
+    public var center: Vector3 { (min + max) * 0.5 }
+    public var halfExtents: Vector3 { (max - min) * 0.5 }
+
     public init(min: Vector3, max: Vector3) {
         self.min = min
         self.max = max
+    }
+
+    public init(center: Vector3, halfExtents: Vector3) {
+        self.min = center - halfExtents
+        self.max = center + halfExtents
     }
 }
