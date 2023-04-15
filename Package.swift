5c0e64f2a5813db2a2cd6d0008faccb0e83e9814
Hongtae Kim <tiff2766@gmail.com>
2023-04-15 15:02:20 +0900

    update package

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DKGame",
-    platforms: [.macOS(.v12), .iOS(.v15), .macCatalyst(.v15)],
+    platforms: [.macOS(.v13), .iOS(.v16), .macCatalyst(.v16)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "DKGame", type: .dynamic, targets: ["DKGame"]),
